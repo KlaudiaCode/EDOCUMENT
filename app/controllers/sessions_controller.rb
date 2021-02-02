3edc10dba3d78670a88cfaecfdfe1027f89c436b
Klaudia <oposyyy@gmail.com>
2021-02-02 19:24:16 +0100

    Added seeding user and admin, fixed session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(usernum: params[:session][:usernum].downcase)
+        user = User.find_by(usernum: params[:session][:usernum])
         if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
             session[:user_num] = user.usernum
