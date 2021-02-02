36f40686e1046ad957b71161ecba528d1f74f550
Klaudia <oposyyy@gmail.com>
2021-02-02 19:12:34 +0100

    Add user role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_31_175046) do
+ActiveRecord::Schema.define(version: 2021_02_02_181032) do
 
   create_table "documents", force: :cascade do |t|
     t.integer "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_01_31_175046) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "role"
   end
 
   add_foreign_key "documents", "users"
