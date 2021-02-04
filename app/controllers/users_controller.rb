0511c4fe90325dcd8290c1eb2d020101b0b5088e
Klaudia <oposyyy@gmail.com>
2021-02-04 10:37:42 +0100

    Add admin authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,24 @@ class UsersController < ApplicationController
     def index
         @users = User.all
     end
+    
     def new
         @user = User.new
     end
+
     def show
         @user = User.find(params[:id])
         if @user.id != current_user.id
             flash[:notice] = "Illegal action"
             redirect_to root_path
+        end  
+        if admin?
+            @documents = Document.all
+        else 
+            @documents = Document.where(user_id: current_user.id)
         end
-        @documents = Document.all
     end
+
     def create
         @user = User.new(user_params)
         if @user.save
